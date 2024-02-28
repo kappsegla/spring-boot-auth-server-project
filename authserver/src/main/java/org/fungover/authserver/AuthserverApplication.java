8b6dbd100fb9c094e15a32d63608658d33d9f8f2
Martin Blomberg <martin.blomberg@outlook.com>
2024-02-28 19:12:17 +0100

    Get all settings right

--- authserver/src/main/java/org/fungover/authserver/AuthserverApplication.java
+++ authserver/src/main/java/org/fungover/authserver/AuthserverApplication.java
@@ -3,8 +3,17 @@ package org.fungover.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 public class AuthserverApplication {
@@ -14,7 +23,7 @@ public class AuthserverApplication {
 	}
 
 	@Bean
-	InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+	UserDetailsService inMemoryUserDetailsManager(){
 		var one = User.withDefaultPasswordEncoder().username("one").roles("admin","user").password("pw").build();
 		var two = User.withDefaultPasswordEncoder().username("two").roles("user").password("pw").build();
 
--- oauthclient/src/main/java/org/fungover/oauthclient/OauthclientApplication.java
+++ oauthclient/src/main/java/org/fungover/oauthclient/OauthclientApplication.java
@@ -2,6 +2,10 @@ package org.fungover.oauthclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OauthclientApplication {
@@ -10,4 +14,14 @@ public class OauthclientApplication {
         SpringApplication.run(OauthclientApplication.class, args);
     }
 
+    @Bean
+    RouteLocator gateway(RouteLocatorBuilder rlb) {
+        return rlb
+                .routes()
+                .route(rs -> rs
+                        .path("/")
+                        .filters(GatewayFilterSpec::tokenRelay)
+                        .uri("http://localhost:8081")
+                ).build();
+    }
 }
