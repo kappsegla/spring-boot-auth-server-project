7a95809bc55ce934567eded33780ce4849c1dd36
Martin Blomberg <martin.blomberg@outlook.com>
2024-02-28 15:40:20 +0100

    Update authserver

--- authserver/src/main/java/org/fungover/authserver/AuthserverApplication.java
+++ authserver/src/main/java/org/fungover/authserver/AuthserverApplication.java
@@ -2,6 +2,9 @@ package org.fungover.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class AuthserverApplication {
@@ -10,4 +13,11 @@ public class AuthserverApplication {
 		SpringApplication.run(AuthserverApplication.class, args);
 	}
 
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+		var one = User.withDefaultPasswordEncoder().username("one").roles("admin","user").password("pw").build();
+		var two = User.withDefaultPasswordEncoder().username("two").roles("user").password("pw").build();
+
+		return new InMemoryUserDetailsManager(one,two);
+	}
 }
