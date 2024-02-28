7f7a352616da9f2dabca9a508d43b53da91bc951
Martin Blomberg <martin.blomberg@outlook.com>
2024-02-28 15:40:50 +0100

    Add resourceserver

---
+++ resourceserver/src/test/java/org/fungover/resourceserver/ResourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fungover.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
